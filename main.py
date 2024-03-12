5d39d8184fd108102be05ed94cf771e80b833b59
mirinai <154983447+mirinai@users.noreply.github.com>
2024-03-12 21:59:17 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import test_module as test
+
+radius = test.number_input()#불러냄
+
+print("원의 둘레 :", test.get_circumference(radius))
+print("원의 넓이 :", test.get_circle_area(radius))
+'''
+숫자입력 :4
+원의 둘레 : 25.132736
+원의 넓이 : 50.265472
+'''
---
+++ test_module.py
@@ -0,0 +1,10 @@
+PI = 3.141592
+
+def number_input():
+    output = input("숫자입력 :")
+    return float(output)
+
+def get_circumference(radius):
+    return 2 * PI * radius
+def get_circle_area(radius):
+    return PI * radius * radius

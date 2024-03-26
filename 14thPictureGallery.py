f3ffaca382fee23b6ca3105b54c13489cc5df203
mirinai <154983447+mirinai@users.noreply.github.com>
2024-03-26 19:59:30 +0900

    14thPictureGallery.py

--- 14thPictureGallery.py
+++ 14thPictureGallery.py
@@ -1,35 +1,48 @@
 from tkinter import *
 from time import *
 
-#전역변수 선언 파트
-fNameList=["jeju1.gif","jeju2.gif","jeju3.gif",
-           "jeju4.gif","jeju5.gif","jeju6.gif","jeju7.gif","jeju8.gif","jeju9.gif",]
+#전역변수 선언 부분
+fnameList = ["jeju1.gif", "jeju2.gif", 
+             "jeju3.gif", "jeju4.gif", 
+             "jeju5.gif", "jeju6.gif", 
+             "jeju7.gif", "jeju8.gif", 
+             "jeju9.gif"]
 
-photoList=[None]*9
+photoList = [None] * 9
 num = 0
 
 def clickNext():
-    global num 
-    num+=1
-    if num>8:
-        num=0
-    photo = PhotoImage(file="gif/"+fNameList[num])
-    pLabel.configure(image=photo)
-    pLabel.image=photo
+  global  num
+  num += 1
+  if num > 8 :
+        num = 0
+  photo = PhotoImage(file = "gif/" + fnameList[num])#PhotoImage 클래스의 객체
+  pLabel.configure(image = photo)
+  pLabel.image = photo
+
+
 def clickPrev():
-    global num 
-    num-=1
-if __name__=="__main__":
-    window=Tk()
-    window.geometry("700x500")
-    window.title("seeing picture album")
-    btnPrev = Button(window, text="<<이전",command=clickPrev)
-    btnNext = Button(window, text="다음>>",command=clickNext)
-    
-    photo = PhotoImage(file="gif/"+fNameList[0])
-    pLabel = Label(window, image=photo)
-    
-    btnPrev.place(x=250,y=10)
-    btnNext.place(x=400,y=10)
-    pLabel.place(x=15,y=50)
-    window.mainloop()
+  global  num
+  num -= 1
+  if num < 0 :
+    num=8
+  photo = PhotoImage(file = "gif/" + fnameList[num])
+  pLabel.configure(image = photo)
+  pLabel.image = photo
+  
+
+if __name__ == "__main__":
+  window = Tk()
+  window.geometry("700x500")
+  window.title("사진 앨범 보기")
+  btnPrev = Button(window, text="<<이전" , command=clickPrev )
+  btnNext = Button(window , text="다음 >>" ,command=clickNext )
+
+  photo = PhotoImage(file="gif/" + fnameList[0])
+  pLabel = Label(window, image=photo)
+
+  btnPrev.place(x = 250, y = 10)
+  btnNext.place(x = 400, y = 10)
+  pLabel.place(x = 15, y = 50)
+
+  window.mainloop()
